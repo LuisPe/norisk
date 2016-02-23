127b0a7feafb2c4704ee3da50d715c658cafb3d1
LuisPe <luis_toloy@hotmail.com>
2016-02-23 14:33:13 -0300

    Redireccionamiento a / si no tiene privilegios

--- app/routes/productos.server.routes.js
+++ app/routes/productos.server.routes.js
@@ -3,15 +3,16 @@
 module.exports = function(app) {
 	var productos = require('../controllers/productos.server.controller');
 	var users = require('../controllers/users.server.controller');
+	var apiAuth = require('../controllers/api.authorization.server.controller');
 
 	app.route('/productos')
 		.get(productos.list)
-		.post(productos.create);
+		.post(apiAuth, users.requiresLogin, productos.create);
 
 	app.route('/productos/:productoId')
 		.get(productos.read)
-		.put(productos.update)
-		.delete(productos.delete);
+		.put(apiAuth, users.requiresLogin, productos.update)
+		.delete(apiAuth, users.requiresLogin, productos.delete);
 
 	// Finish by binding the article middleware
 	app.param('productoId', productos.getByID);

a994ea6d3d11b4c9f226075edb923c25c76c9406
Victordgrandis <victordg91@gmail.com>
2016-02-26 11:12:16 -0300

    Cierre de Barra Lateral

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -5,6 +5,9 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 		$scope.authentication = Authentication;
 		$scope.isCollapsed = false;
 		$scope.menu = Menus.getMenu('topbar');
+	    var trigger = $('.icono'),
+	         overlay = $('.overlay'),
+	         isClosed = false;
 
 		$scope.toggleCollapsibleMenu = function() {
 			$scope.isCollapsed = !$scope.isCollapsed;
@@ -13,6 +16,27 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 		// Collapsing the menu after navigation
 		$scope.$on('$stateChangeSuccess', function() {
 			$scope.isCollapsed = false;
+		});
+
+	        
+        function icono_cross() {
+
+          if (isClosed == true) {          
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+          } else {   
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+          }
+      	}
+	      
+		$('[data-toggle="offcanvas"]').click(function () {
+		    $('#wrapper').toggleClass('toggled');
+		    icono_cross();
 		});
 	}
 ]);

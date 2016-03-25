59fcbe7f90325cd70bcd301708797d783697847f
LuisPe <luis_toloy@hotmail.com>
2016-03-25 01:14:38 -0300

    CorrecciónII Producción

--- config/env/production.js
+++ config/env/production.js
@@ -34,16 +34,9 @@ module.exports = {
 				'public/lib/vegas/vegas.js',
 				'public/lib/toastr/toastr.min.js'
 			]
-		},
-		css: [
-			'public/modules/**/css/*.css'
-		],
-		js: [
-			'public/config.js',
-			'public/application.js',
-			'public/modules/*/*.js',
-			'public/modules/*/*[!tests]*/*.js'
-		]
+		},,
+		css: 'public/dist/application.min.css',
+		js: 'public/dist/application.min.js'
 	},
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
